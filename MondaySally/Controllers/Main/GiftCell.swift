848163e6b464180531f5963d935effad33611391
Youngminah <bluebelsmudi@gmail.com>
2021-07-04 19:21:12 +0900

    Add: 이미지 에셋 추가 , Update: 기프트샵 UI완료

---
+++ MondaySally/Controllers/Main/GiftCell.swift
@@ -0,0 +1,12 @@
+//
+//  GiftCell.swift
+//  MondaySally
+//
+//  Created by meng on 2021/07/04.
+//
+
+import UIKit
+
+class GiftCell: UICollectionViewCell {
+    
+}
--- MondaySally/Controllers/Main/GiftTabViewController.swift
+++ MondaySally/Controllers/Main/GiftTabViewController.swift
@@ -11,19 +11,31 @@ class GiftTabViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension GiftTabViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GiftCell", for: indexPath) as? GiftCell else {
+            return UICollectionViewCell()
+        }
 
+        return cell
+    }
+    
+    //UICollectionViewDelegateFlowLayout 프로토콜
+    //cell사이즈를  계산할꺼 - 다양한 디바이스에서 일관적인 디자인을 보여주기 위해 에 대한 답
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = (collectionView.bounds.width - 16)/2
+        let height: CGFloat = width * 265/200
+        return CGSize(width: width, height: height)
+    }
+    
 }
