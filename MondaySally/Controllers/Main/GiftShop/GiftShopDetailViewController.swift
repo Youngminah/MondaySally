8a101a7c87682db01d0e1d07c37db94d2b205806
Youngminah <bluebelsmudi@gmail.com>
2021-07-16 04:02:57 +0900

    Update: 기프트 신청 API 연동 완료

--- MondaySally/Controllers/Main/GiftShop/GiftShopDetailViewController.swift
+++ MondaySally/Controllers/Main/GiftShop/GiftShopDetailViewController.swift
@@ -9,83 +9,132 @@ import UIKit
 
 class GiftShopDetailViewController: UIViewController {
     
-    let viewModel = GiftDetailViewModel(dataService: DataService())
-    var giftIndex: Int = -1
     @IBOutlet weak var giftNameLabel: UILabel!
     @IBOutlet weak var giftContentLabel: UILabel!
     @IBOutlet weak var giftRuleLabel: UILabel!
+    
+    var input: GiftRequestInput?
+    let giftDetailViewModel = GiftDetailViewModel(dataService: DataService())
+    let giftRequestViewModel = GiftRequestViewModel(dataService: DataService())
+    var giftIndex: Int = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "기프트 샵"
         if giftIndex != -1 {
-            self.attemptFetchGiftList(with: giftIndex)
+            self.attemptFetchGiftDetail(with: giftIndex)
         }
     }
     
     @IBAction func giftApplyButton(_ sender: UIButton) {
-        guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "GiftCompletedView") as? GiftCompletedViewController else{
-            return
-        }
-        self.navigationController?.pushViewController(vc, animated: true)
+        input = GiftRequestInput(giftIdx: giftIndex, usedClover: 30)
+        guard let input = input else { return }
+        attemptFetchGiftRequest(with :input)
     }
     
 }
-// MARK: - Networking
+
+
+// MARK: 기프트 리스트 조회 API
 extension GiftShopDetailViewController {
 
-    //내 프로필 조회 API 호출 함수
-    private func attemptFetchGiftList(with index:Int) {
-        self.viewModel.updateLoadingStatus = {
+    private func attemptFetchGiftDetail(with index:Int) {
+        self.giftDetailViewModel.updateLoadingStatus = {
             DispatchQueue.main.async { [weak self] in
-                guard let strongSelf = self else {
-                    return
-                }
-                let _ = strongSelf.viewModel.isLoading ? strongSelf.showIndicator() : strongSelf.dismissIndicator()
+                guard let strongSelf = self else { return }
+                let _ = strongSelf.giftDetailViewModel.isLoading ? strongSelf.showIndicator() : strongSelf.dismissIndicator()
             }
         }
 
-        self.viewModel.showAlertClosure = { [weak self] () in
+        self.giftDetailViewModel.showAlertClosure = { [weak self] () in
+            guard let strongSelf = self else { return }
             DispatchQueue.main.async {
-                if let error = self?.viewModel.error {
+                if let error = strongSelf.giftDetailViewModel.error {
                     print("서버에서 통신 원활하지 않음 -> \(error.localizedDescription)")
-                    self?.networkFailToExit()
+                    strongSelf.networkFailToExit()
                 }
-                if let message = self?.viewModel.failMessage {
+                if let message = strongSelf.giftDetailViewModel.failMessage {
                     print("서버에서 알려준 에러는 -> \(message)")
+                    strongSelf.dismiss(animated: true)
                 }
             }
         }
         
-        self.viewModel.logOutAlertClosure = { [weak self] () in
-            guard let strongSelf = self else {
-                return
-            }
+        self.giftDetailViewModel.logOutAlertClosure = { [weak self] () in
+            guard let strongSelf = self else { return }
             DispatchQueue.main.async {
 
             }
         }
 
-        self.viewModel.didFinishFetch = { [weak self] () in
-            
+        self.giftDetailViewModel.didFinishFetch = { [weak self] () in
+            guard let strongSelf = self else { return }
             DispatchQueue.main.async {
-                guard let strongSelf = self else {
-                    return
-                }
                 print("기프트 상세 조회에 성공했습니다 !! ")
                 strongSelf.updateGiftDetailUI()
             }
         }
 
-        self.viewModel.fetchGiftDetail(with: index)
+        self.giftDetailViewModel.fetchGiftDetail(with: index)
     }
     
     private func updateGiftDetailUI(){
-        guard let data = self.viewModel.getGiftInfo else {
+        guard let data = self.giftDetailViewModel.getGiftInfo else {
             print("디테일 기프트 정보를 뜯지 못했습니다.")
             return
         }
         self.giftNameLabel.text = data.name
+        //self.giftContentLabel.text = data.info
+        //self.giftRuleLabel.text = data.rule
+    }
+}
+
+
+// MARK: 기프트 신청 API
+extension GiftShopDetailViewController {
+    private func attemptFetchGiftRequest(with input: GiftRequestInput) {
+        self.giftRequestViewModel.updateLoadingStatus = {
+            DispatchQueue.main.async { [weak self] in
+                guard let strongSelf = self else { return }
+                let _ = strongSelf.giftRequestViewModel.isLoading ? strongSelf.showIndicator() : strongSelf.dismissIndicator()
+            }
+        }
+
+        self.giftRequestViewModel.showAlertClosure = { [weak self] () in
+            guard let strongSelf = self else { return }
+            DispatchQueue.main.async {
+                if let error = strongSelf.giftRequestViewModel.error {
+                    print("서버에서 통신 원활하지 않음 -> \(error.localizedDescription)")
+                    strongSelf.networkFailToExit()
+                }
+                if let message = strongSelf.giftRequestViewModel.failMessage {
+                    print("서버에서 알려준 에러는 -> \(message)")
+                }
+            }
+        }
+        
+        self.giftRequestViewModel.logOutAlertClosure = { [weak self] () in
+            guard let strongSelf = self else { return }
+            DispatchQueue.main.async {
+
+            }
+        }
+
+        self.giftRequestViewModel.didFinishFetch = { [weak self] () in
+            DispatchQueue.main.async {
+                guard let strongSelf = self else { return }
+                print("기프트 신청에 성공했습니다 !! ")
+                strongSelf.moveToCompletedView()
+            }
+        }
+
+        self.giftRequestViewModel.fetchGiftRequest(with: input)
     }
     
+    private func moveToCompletedView(){
+        guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "GiftCompletedView") as? GiftCompletedViewController else{
+            return
+        }
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
--- MondaySally/Models/GiftRequestInput.swift
+++ MondaySally/Models/GiftRequestInput.swift
@@ -9,7 +9,6 @@ struct GiftRequestInput: Encodable {
     var giftIdx: Int
     var usedClover: Int
     
-    
     var toDictionary: [String: Any] {
         let dict: [String: Any]  = ["giftIdx": giftIdx, "usedClover": usedClover]
         return dict
